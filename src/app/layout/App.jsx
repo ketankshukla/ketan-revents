33b8ad65505c88842adb7535ef3ec8d7fe54320b
Ketan Shukla <ketankshukla@gmail.com>
2018-08-14 15:52:02 -0700

    created a folder structure where we will create components by feature

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <h1>Ketan's Revents</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+ 
